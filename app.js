665b3d27734ee9e9d4a733e70aabe70807af2020
devichensky mikhail <vedoone@mail.ru>
2022-07-04 00:46:59 +0700

    tsc

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+// let cevents: number = 10000;
+// let bonus: number = 500;
+// let resolve: number = cevents + bonus;
+// console.log('resolve :>> ', resolve);
+// ---------------------------------------------------------------------------
+/**
+ * явная типизация
+ * @param firstName
+ * @param surName
+ * @returns
+ */
+function getFullName(firstName, surName) {
+    return `${firstName} ${surName}`;
+}
+// const getFullName = (firstName: string, surName: string): string => {
+//     return firstName + surName;
+// };
+console.log(getFullName('Маша', 'Петрова'));
---
+++ app.ts
@@ -0,0 +1,23 @@
+// let cevents: number = 10000;
+// let bonus: number = 500;
+// let resolve: number = cevents + bonus;
+// console.log('resolve :>> ', resolve);
+// ---------------------------------------------------------------------------
+// типы и функции
+// /**
+//  * явная типизация
+//  * @param firstName 
+//  * @param surName 
+//  * @returns 
+//  */
+// function getFullName(firstName:string, surName:string): string {
+//     return `${firstName} ${surName}`;
+// }
+
+// // const getFullName = (firstName: string, surName: string): string => {
+// //     return firstName + surName;
+// // };
+// console.log(getFullName('Маша', 'Петрова'));
+// ---------------------------------------------------------------------------
+// типизация Обьектов.
+
