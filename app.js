0209f2ac7cb35180ec0d552cfca01ffe18239d52
invector4ik002 <vedoone@mail.ru>
2022-07-05 17:05:33 +0700

    Tuples, Redonly, Enums

--- app.js
+++ app.js
@@ -9,31 +9,31 @@
 // console.log('res :>> ', res);
 // ================================================================
 // использование типов в функциях
-// /**
-//  * протипизированна string
-//  * @param firstname string
-//  * @param surname string
-//  * @returns string
-//  */
+/**
+ * протипизированна string
+ * @param firstname string
+ * @param surname string
+ * @returns string
+ */
 // function getFullName(firstname: string, surname: string): string {
 //     return `${firstname} + ${surname}`
 // }
-// /**
-//  * пример стрелочной фуеции
-//  * протипизированна string
-//  * @param firstname 
-//  * @param surname 
-//  * @returns 
-//  */
+/**
+ * пример стрелочной фуеции
+ * протипизированна string
+ * @param firstname
+ * @param surname
+ * @returns
+ */
 // const getFullNameArrow = (firstname: string, surname: string): string => {
 //     return `${firstname} + ${surname}`
 // }
-// /**
-//  * пример без явной/строгой типизации
-//  * @param firstname any/любой тип
-//  * @param surname any/любой тип
-//  * @returns any/любой тип
-//  */
+/**
+ * пример без явной/строгой типизации
+ * @param firstname any/любой тип
+ * @param surname any/любой тип
+ * @returns any/любой тип
+ */
 // function getFullName(firstname: any, surname: any):any {
 //     if(firstname !== 'string') {
 //         throw new Error('!!!!')
@@ -54,13 +54,157 @@
 // console.log('getFullName :>> ', getFullName('Devichensky', 'Michail'));
 // =============================================================================
 // Типизация обьектов
-const user = {
-    firstname: 'Devichensky',
-    surname: 'Michail',
-    city: 'Novosibirsk',
-    age: 41,
+// const user = {
+//     firstname: 'Devichensky',
+//     surname: 'Michail',
+//     city: 'Novosibirsk',
+//     age: 41,
+//     skills: {
+//         dev: true,
+//         devops: true,
+//     }
+// };
+// function getFullName(userEntity: { firstname: string, surname: string }): string {
+//     return `${userEntity.firstname} ${userEntity.surname}`
+// }
+// console.log('getFullName :>> ', getFullName(user));
+// ===============================================================================
+// упражнение типизируем обьект
+// let office = {
+//     "officeId": 45,
+//     "isOpened": false,
+//     "contacts": {
+//         "phone": "+79100000000",
+//         "email": "my@email.ru",
+//         "address": {
+//             "city": "Москва"
+//         }
+//     }
+// }
+// let officeType: {
+//     officeId: number;
+//     isOpened: boolean;
+//     contacts: {
+//         phone: string;
+//         email: string;
+//         address: {
+//             city: string;
+//         };
+//     };
+// }
+// =====================================================================
+//  types in arrays
+// const skills = ['dev', 'DevOps']; 
+/**
+ * явное типизирование массива
+ */
+// const skills: string[] = ['dev', 'DevOps', "Testing"];
+// const res = skills
+// .filter(element => element !== 'DevOps')
+// .map(element => element + `${'!'}`)
+// console.log('res :>> ', res);
+// =======================================================================
+// Tuples/картеж
+/**
+ * Tuples/картеж решает проблему с явно типизированным массивом в котором есть
+ * другие типы данных
+ * Tuple это массив ограниченной длинны в котором каждый элемент типизирован
+ * Tuples не запрещает работат таким методам массива как pop, push
+ * но к добавленным элементам нельзя обратиться
+ * Tuples не запрещает работать синтаксису диструктуризации массива
+ */
+// const skills: [string,  number] = ['dev', 1];
+// const [id, skillName] = skills;
+// console.log('id :>> ', id);
+// console.log('skillName :>> ', skillName);
+// // использование спред оператора в Tuples
+// const array: [string,  number, ...boolean[]] = ['dev', 1, true, true, true]
+// ==============================================================================
+// Readonly/Только для чтения
+/**
+ * Readonly преднозначен для модификации массива которая запрещает какие либо изманения с данными
+ * Ошибками становятся все методы по модификации масива или синтаксиса перназначения
+ * Только для чтения
+ */
+// const skills: readonly [string,  number] = ['dev', 1];
+// skills[1] = 2;
+// skills.push('!');
+// ============================================================================ 
+// Enums/перечисления
+// const res = {
+//     message: 'Платеж успешен',
+//     statusCode: 'success',
+// }
+/**
+ * Проблему опечаток можно решить через описания в константы
+ * минусы в этом, это кол-во констант в которых то же с ростом проектов
+ * происходит путаница и раздутия кода
+ */
+// const SUCCESS = 'success';
+/**
+ * По причине ошибки в написании Success с загалавной проверка вернет error
+ *
+ */
+// if(res.statusCode === 'Success') {
+//     console.log('success :>> ', 'success');
+// } else {
+//     throw new Error('error');
+// }
+// Пример решения с применением enum
+// Пример условий
+// 1- успех
+// 2 - в процессе
+// 3 - откланен 
+/**
+ * Можно присвоить любые значения enum
+ * Рассмотрин ЧИСЛОВОЙ enum
+ */
+// enum StatusCode {
+//     SUCCSESS = 1,
+//     IN_PROCESS,
+//     FAILED,
+// }
+// 's'- успех
+// 'p' - в процессе
+// 'f' - откланен
+/**
+ * Можно присвоить любые значения enum
+ * Рассмотрин СТРОКОВОЙ enum
+ */
+// enum StatusCode {
+//     SUCCSESS = 's',
+//     IN_PROCESS='p',
+//     FAILED='f',
+// }
+//  1 - успех
+// 'p' - в процессе
+// 'f' - откланен
+/**
+ * Можно присвоить любые значения enum
+ * Рассмотрин Гидрогенный enum
+ */
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCSESS"] = 1] = "SUCCSESS";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Платеж успешен',
+    statusCode: StatusCode.SUCCSESS
 };
-function getFullName(userEntity) {
-    return `${userEntity.firstname} ${userEntity.surname}`;
+// if(res.statusCode === StatusCode.SUCCSESS) {
+//     console.log('res.statusCode :>> ', 'Платеж успешен');
+// } else {
+//     throw new Error('error');
+// }
+function action(status) {
+    if (res.statusCode === StatusCode.SUCCSESS) {
+        console.log('res.statusCode :>> ', 'Платеж успешен');
+    }
+    else {
+        throw new Error('error');
+    }
 }
-console.log('getFullName :>> ', getFullName(user));
+// action(StatusCode.SUCCSESS);
+action(1);
