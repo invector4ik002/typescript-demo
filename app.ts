9861a86d0cc27d262f1e58879c8a632e039f5bf7
invector4ik002 <vedoone@mail.ru>
2022-07-05 09:30:09 +0700

    material from a file(app.ts) from the second machine

--- app.ts
+++ app.ts
@@ -1,23 +1,79 @@
-// let cevents: number = 10000;
-// let bonus: number = 500;
-// let resolve: number = cevents + bonus;
-// console.log('resolve :>> ', resolve);
-// ---------------------------------------------------------------------------
-// типы и функции
+// Простые типы
+// let revenue: number= 1000;
+// let bonus: number= 500;
+
+// let string: string = 'string';
+// let booleanTru: boolean = true;
+// let booleanFal: boolean = false;
+
+// let res: number = revenue + bonus;
+// console.log('res :>> ', res);
+
+// ================================================================
+
+// использование типов в функциях
 // /**
-//  * явная типизация
-//  * @param firstName 
-//  * @param surName 
-//  * @returns 
+//  * протипизированна string
+//  * @param firstname string
+//  * @param surname string
+//  * @returns string
 //  */
-// function getFullName(firstName:string, surName:string): string {
-//     return `${firstName} ${surName}`;
+// function getFullName(firstname: string, surname: string): string {
+//     return `${firstname} + ${surname}`
 // }
 
-// // const getFullName = (firstName: string, surName: string): string => {
-// //     return firstName + surName;
-// // };
-// console.log(getFullName('Маша', 'Петрова'));
-// ---------------------------------------------------------------------------
-// типизация Обьектов.
+// /**
+//  * пример стрелочной фуеции
+//  * протипизированна string
+//  * @param firstname 
+//  * @param surname 
+//  * @returns 
+//  */
+// const getFullNameArrow = (firstname: string, surname: string): string => {
+//     return `${firstname} + ${surname}`
+// }
+
+// /**
+//  * пример без явной/строгой типизации
+//  * @param firstname any/любой тип
+//  * @param surname any/любой тип
+//  * @returns any/любой тип
+//  */
+// function getFullName(firstname: any, surname: any):any {
+
+//     if(firstname !== 'string') {
+//         throw new Error('!!!!')
+//     }
+
+//     return `${firstname} + ${surname}`
+// }
+// console.log('getFullName :>> ', getFullName(true, false));
+
+// /**
+//  * пример с явной/строгой типизацией
+//  * @param firstname string
+//  * @param surname string
+//  * @returns string
+//  * Вывод как минимум сокращает код :)
+//  */
+// function getFullName(firstname: string, surname: string):string {
+//     return `${firstname} ${surname}`
+// }
+// console.log('getFullName :>> ', getFullName('Devichensky', 'Michail'));
+
+// =============================================================================
+
+// Типизация обьектов
+
+const user = {
+    firstname: 'Devichensky',
+    surname: 'Michail',
+    city: 'Novosibirsk',
+    age: 41,
+};
+
+function getFullName(userEntity):string {
+    return `${userEntity.firstname} ${userEntity.surname}`
+}
+console.log('getFullName :>> ', getFullName(user));
 
