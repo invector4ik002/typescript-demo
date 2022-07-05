01281ce3a4a0a9609925c31db4bafe936fc32e31
invector4ik002 <vedoone@mail.ru>
2022-07-05 09:27:21 +0700

    material from a file from the second machine

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 "use strict";
-// let cevents: number = 10000;
-// let bonus: number = 500;
-// let resolve: number = cevents + bonus;
-// console.log('resolve :>> ', resolve);
-// ---------------------------------------------------------------------------
-/**
- * явная типизация
- * @param firstName
- * @param surName
- * @returns
- */
+// Простые типы
+// let revenue: number= 1000;
+// let bonus: number= 500;
+// let string: string = 'string';
+// let booleanTru: boolean = true;
+// let booleanFal: boolean = false;
+// let res: number = revenue + bonus;
+// console.log('res :>> ', res);
+// ================================================================
+// использование типов в функциях
+// /**
+//  * протипизированна string
+//  * @param firstName string
+//  * @param surName string
+//  * @returns string
+//  */
+// function getFullName(firstName: string, surName: string): string {
+//     return `${firstName} + ${surName}`
+// }
 function getFullName(firstName, surName) {
-    return `${firstName} ${surName}`;
+    if (firstName !== 'string') {
+        throw new Error('!!!!!!!');
+    }
+    return `${firstName} + ${surName}`;
 }
-// const getFullName = (firstName: string, surName: string): string => {
-//     return firstName + surName;
-// };
-console.log(getFullName('Маша', 'Петрова'));
-// -------------------------------------------------------------------------------
-// типизация обьектов
+console.log('getFullName :>> ', getFullName(true, false));
+// /**
+//  * пример стрелочной фуеции
+//  * протипизированна string
+//  * @param firstName 
+//  * @param surName 
+//  * @returns 
+//  */
+// const getFullNameArrow = (firstName: string, surName: string): string => {
+//     return `${firstName} + ${surName}`
+// }
