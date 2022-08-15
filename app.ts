9d55e6938d8b4724be4328c244a4a3b4b0bc58f4
invector4ik002 <vedoone@mail.ru>
2022-08-15 16:34:12 +0700

    visibility of properties

--- app.ts
+++ app.ts
@@ -1617,3 +1617,56 @@
 //     this.payment = payment;
 //   }
 // }
+
+// ============================== Видимость св-в =============================
+/**
+ * Пример
+ */
+class Vehicle {
+    make: string; /* Публичное св-во, аналогичная запись с приставкой public make: string и доступно для изменений из вне new Vehicle().make = 'b';*/
+    private damage: string[]; /* приватное св-во закрыто для изменений из вне то есть при вызове обьекта синтаксис точки не покажет это св-во как имеющееся.
+        Приватным св-вам доступны изменения только внутри класса. Пример метод addDamages()
+     */
+
+    private _model: string; /* Пример приватного свойства во взаимодействии через get и set */
+
+    protected run: number;/* Третий модификатор св-в protected  */
+    /**
+     * 
+     */
+    set model(model:string) {
+        this._model = model;
+    }
+    /**
+     * Через get вполне можно изменять privat свойство обьекта 
+     * Метод доступен при обьявлении обьекта const modelVehicle = new Vehicle(); modelVehicle.model = 'UAZ'
+     */
+    get model(){
+        return this._model
+    }
+
+    /**
+     * @function addDamages может изменить приватное св-во damage
+     * @param damage : string
+     */
+    addDamages(damage: string) {
+        this.damage.push(damage);
+    };
+};
+/**
+ * class EuroTruck какойто еврогрузовик наследуется от Vehicle 
+ * он наследует только публичные св-ва обьекта так же метод get и set 
+ * приватные ему не доступны.
+ */
+class EuroTruck extends Vehicle {
+    /**
+     * то что доступно при наследовании
+     * this.setDamage
+     * this.make
+     * this.model
+     * this.addDamages
+     */
+    setDamage(){
+        this.model
+    }
+}
